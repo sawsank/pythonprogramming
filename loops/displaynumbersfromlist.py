daabe7c2616c15ad98c03f771848170eed8c29cb
sawsank <shasank3joshi77@gmail.com>
2025-09-24 19:58:01 +0545

    commit

---
+++ loops/displaynumbersfromlist.py
@@ -0,0 +1,10 @@
+numbers = [12, 75, 150, 180, 145, 525, 50]
+
+for item in numbers:
+    if item>500:
+        break
+    elif item>150:
+        continue
+
+    elif item % 5 == 0:
+        print(item)
---
+++ loops/sumofallnumbers.py
@@ -0,0 +1,7 @@
+s = 0
+n = int(input("Enter a number "))
+
+for i in range(1, n + 1, 1):
+    s += i
+print("\n")
+print("Sum is: ", s)
